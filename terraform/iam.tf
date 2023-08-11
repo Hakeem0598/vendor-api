730640e3f5c58a9ebd80d2a605dc4ef11dde79b9
Hakeem0598 <ayo75@icloud.com>
2023-08-11 14:06:34 +0100

    Update service id

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
     effect  = "Allow"
     principals {
       type        = "Service"
-      identifiers = ["lambda.amamzonaws.com"]
+      identifiers = ["lambda.amazonaws.com"]
     }
   }
 }
